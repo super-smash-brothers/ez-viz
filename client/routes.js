787af106fd64e1b6bf4badc0a298e6f4cf481e7b
Eric Yang <yangeric12@gmail.com>
2020-01-31 15:51:27 -0500

    fix headless pointer to ReactMap

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,6 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
-import {ReactMap} from './components/reactmap'
 
 /**
  * COMPONENT
@@ -29,8 +28,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        {/* <Route component={Login} /> */}
-        <Route component={ReactMap} />
+        <Route component={Login} />
       </Switch>
     )
   }
