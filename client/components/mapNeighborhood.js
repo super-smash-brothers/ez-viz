4e1d9a57260dd4bb603b941bac8e34aa20bef45e
Eric Yang <yangeric12@gmail.com>
2020-02-05 16:11:54 -0500

    display MultiPolygon neighborhoods

--- client/components/mapNeighborhood.js
+++ client/components/mapNeighborhood.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import * as d3 from 'd3'
 
-//put a single neighborhood's coordinates in a json to use
-// console.log('d3', d3)
 export const MapNeighborhood = props => {
-  // console.log('props: ', props.neighborhood.geometry.coordinates[0])
   const {line, xScale, yScale, neighborhood, width, height} = props
 
   if (neighborhood.geometry.type === 'MultiPolygon') {
-    return neighborhood.geometry.coordinates[0].map(lines => {
-      console.log('lines: ', line(lines[0][0]))
+    return neighborhood.geometry.coordinates.map(singlePolygon => {
       return (
-        <path d={line(lines)} strokeWidth="2" fill="none" stroke="#eb6a5b" />
+        <path
+          d={line(singlePolygon[0])}
+          strokeWidth="2"
+          fill="none"
+          stroke="#eb6a5b"
+        />
       )
     })
   }
+
   return (
     <path
       d={line(neighborhood.geometry.coordinates[0])}
