63fc424e7e57818c3f398169706a8c408e204430
zkarmi <55897726+zkarmi@users.noreply.github.com>
2020-02-11 17:54:22 -0500

    Update population.js

--- server/db/models/population.js
+++ server/db/models/population.js
@@ -1,10 +1,3 @@
-const mongoose = require('mongoose')
-process.env.MONGODB_SERVER = `mongodb+srv://viZ:UnaZiv4JavaMineN0ir@cluster0-hqnuy.mongodb.net/delvenyc`
-const connURI = process.env.MONGODB_SERVER
-  ? process.env.MONGODB_SERVER
-  : 'mongodb://localhost/delvenyc'
-console.log('Connection URI:', connURI)
-
 const db = mongoose.connect(connURI, {useNewUrlParser: true})
 
 const PopulationSchema = new mongoose.Schema({
